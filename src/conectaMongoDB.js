4e5b563d07bcd7ae0e116ebef82733ec75674860
CMeskita <danielle.cmesquita@gmail.com>
2024-02-19 14:59:19 -0300

    Update env

--- src/conectaMongoDB.js
+++
@@ -1,2 +0,0 @@
-
-export const DB_CONNECTION_STRING = "mongodb+srv://daniellecmesquita:MeGtSKpKeucMdqRm@cluster0.9ck4koq.mongodb.net/tarefa?retryWrites=true&w=majority"
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,8 +1,9 @@
 import mongoose,{mongo} from "mongoose";
+import 'dotenv/config'
 
 async function DataBaseTarefas(){
 
-mongoose.connect("mongodb+srv://daniellecmesquita:MeGtSKpKeucMdqRm@cluster0.9ck4koq.mongodb.net/tarefa?retryWrites=true&w=majority");
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.9ck4koq.mongodb.net/tarefa?retryWrites=true&w=majority`);
 return mongoose.connection;
 
 }
